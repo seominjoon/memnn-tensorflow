d8109a6a106b94efce5fb6d4aa7abe539dfae577
Minjoon Seo <seominjoon@gmail.com>
2016-01-21 16:15:18 -0800

    data fixed, working on n2n model

--- memnn/data.py
+++ memnn/data.py
@@ -4,15 +4,16 @@ import logging
 
 
 class DataSet(object):
-    def __init__(self, ms, qs, ys, vocab_size=None):
-        assert len(ms) == len(ys), "X and Y sizes don't match."
-        self._num_examples = len(ms)
-        self.ms = ms
+    def __init__(self, xs, qs, ys, vocab_size=None, vocab_map=None):
+        assert len(xs) == len(ys), "X and Y sizes don't match."
+        self._num_examples = len(xs)
+        self.xs = xs
         self.qs = qs
         self.ys = ys
         self.vocab_size = vocab_size
-        self.max_m_len = max(len(m) for m in ms)
-        self.max_s_len = max(len(mi) for m in ms for mi in m)
+        self.vocab_map = vocab_map
+        self.max_m_len = max(len(m) for m in xs)
+        self.max_s_len = max(len(mi) for m in xs for mi in m)
         self.max_q_len = max(len(q) for q in qs)
         self._epochs_completed = 0
         self._index_in_epoch = 0
@@ -23,7 +24,7 @@ class DataSet(object):
             self._index_in_epoch = 0
             self._epochs_completed += 1
         i, b = self._index_in_epoch, batch_size
-        batch = self.ms[i:i + b], self.qs[i:i + b], self.ys[i:i + b]
+        batch = self.xs[i:i + b], self.qs[i:i + b], self.ys[i:i + b]
         self._index_in_epoch += batch_size
         return batch
 
@@ -32,9 +33,11 @@ def _tokenize(raw):
     return re.findall(r"[\w']+|[.,!?;]", raw)
 
 
+_s_re = re.compile("^(\\d+) ([\\w\\s.]+)")
+_q_re = re.compile("^(\\d+) ([\\w\\s\\?]+)\t([\\w,]+)\t(\\d+)")
+
+
 def read_babi_files(file_paths):
-    s_re = re.compile("^(\\d+) ([\\w\\s.]+)")
-    q_re = re.compile("^(\\d+) ([\\w\\s\\?]+)\t([\\w,]+)\t(\\d+)")
 
     vocab_set = set()
     paragraphs = []
@@ -46,8 +49,8 @@ def read_babi_files(file_paths):
             lines = fh.readlines()
             paragraph = []
             for line_num, line in enumerate(lines):
-                sm = s_re.match(line)
-                qm = q_re.match(line)
+                sm = _s_re.match(line)
+                qm = _q_re.match(line)
                 if qm:
                     id_, raw_question, answer, support = qm.groups()
                     question = _tokenize(raw_question)
@@ -67,14 +70,25 @@ def read_babi_files(file_paths):
                     logging.error("Invalid line encountered: line %d in %s" % (line_num + 1, file_path))
             print "Loaded %d examples from: %s" % (len(paragraphs), os.path.basename(file_path))
 
-    vocab_map = dict((v, k) for k, v in enumerate(sorted(vocab_set)))
-    xs = [[[vocab_map[word] for word in sentence] for sentence in paragraph] for paragraph in paragraphs]
-    qs = [[vocab_map[word] for word in question] for question in questions]
-    ys = [vocab_map[answer] for answer in answers]
-    data_set = DataSet(xs, qs, ys, vocab_size=len(vocab_map))
-    data_set.vocab_map = vocab_map
+    return vocab_set, paragraphs, questions, answers
 
-    return data_set
+
+def read_babi_split(*file_paths_list):
+    vocab_set_list, paragraphs_list, questions_list, answers_list = zip(*[read_babi_files(file_paths) for file_paths in file_paths_list])
+    vocab_set = vocab_set_list[0]
+    vocab_map = dict((v, k+1) for k, v in enumerate(sorted(vocab_set)))
+    vocab_map[0] = "UNK"
+
+    def _get(vm, w):
+        if w in vm:
+            return vm[w]
+        return 0
+
+    xs_list = [[[[_get(vocab_map, word) for word in sentence] for sentence in paragraph] for paragraph in paragraphs] for paragraphs in paragraphs_list]
+    qs_list = [[[_get(vocab_map, word) for word in question] for question in questions] for questions in questions_list]
+    ys_list = [[_get(vocab_map, answer) for answer in answers] for answers in answers_list]
+    data_sets = [DataSet(xs, qs, ys, vocab_size=len(vocab_map), vocab_map=vocab_map) for xs, qs, ys in zip(xs_list, qs_list, ys_list)]
+    return data_sets
 
 
 def read_babi_all(dir_path, prefix="", suffix=""):
@@ -86,9 +100,7 @@ def read_babi_all(dir_path, prefix="", suffix=""):
             train_file_paths.append(file_path)
         elif file_name.startswith(prefix) and file_name.endswith(suffix + "_test.txt"):
             test_file_paths.append(file_path)
-    train_data_set = read_babi_files(train_file_paths)
-    test_data_set = read_babi_files(test_file_paths)
-    return train_data_set, test_data_set
+    return read_babi_split(train_file_paths, test_file_paths)
 
 
 def read_babi(size='small', prefix="", suffix=""):
@@ -102,6 +114,8 @@ def read_babi(size='small', prefix="", suffix=""):
 
 
 if __name__ == "__main__":
-    train, test = read_babi(prefix="")
+    train, test = read_babi(prefix="qa1_")
     print train.vocab_size, train.max_m_len, train.max_s_len, train.max_q_len
     print test.vocab_size, test.max_m_len, test.max_s_len, test.max_q_len
+    print train.next_batch(1)
+    print train.next_batch(1)
