306e9871087ef4ecd605ca0154c7e68f550fa631
Minjoon Seo <seominjoon@gmail.com>
2016-02-25 15:56:11 -0800

    changed data reading scheme

--- read_data.py
+++ read_data.py
@@ -5,35 +5,44 @@ from pprint import pprint
 import numpy as np
 
 class DataSet(object):
-    def __init__(self, xs, qs, ys, max_sentence_size=None, vocab_size=None, vocab_map=None):
-        assert len(xs) == len(ys), "X and Y sizes don't match."
+    def __init__(self, batch_size, idxs, xs, qs, ys, include_leftover=False, name=""):
+        assert len(xs) == len(qs) == len(ys), "X, Q, and Y sizes don't match."
+        assert batch_size <= len(xs), "batch size cannot be greater than data size."
+        self.name = name or "dataset"
+        self.idxs = idxs
         self.num_examples = len(xs)
         self.xs = xs
         self.qs = qs
         self.ys = ys
-        self.max_sentence_size = max_sentence_size
-        self.vocab_size = vocab_size
-        self.vocab_map = vocab_map
-        self._epochs_completed = 0
-        self._index_in_epoch = 0
+        self.num_epochs_completed = 0
+        self.idx_in_epoch = 0
+        self.batch_size = batch_size
+        self.include_leftover = include_leftover
+        self.num_batches = self.num_examples / self.batch_size + int(include_leftover)
+        self.reset()
 
-    def next_batch(self, batch_size):
-        assert batch_size <= self.num_examples, "batch size cannot be greater than data size."
-        assert self.has_next(batch_size), "there is not batch left!"
-        i, b = self._index_in_epoch, batch_size
-        batch = self.xs[i:i + b], self.qs[i:i + b], self.ys[i:i + b]
-        self._index_in_epoch += batch_size
-        return batch
+    def get_next_labeled_batch(self):
+        assert self.has_next_batch(), "End of epoch. Call 'complete_epoch()' to reset."
+        from_, to = self.idx_in_epoch, self.idx_in_epoch + self.batch_size
+        if self.include_leftover and to > self.num_examples:
+            to = self.num_examples
+        cur_idxs = self.idxs[from_:to]
+        xs, qs, ys = zip(*[[self.xs[i], self.qs[i], self.ys[i]] for i in cur_idxs])
+        self.idx_in_epoch += self.batch_size
+        return xs, qs, ys
 
-    def has_next(self, batch_size):
-        return self._index_in_epoch + batch_size <= self.num_examples
+    def has_next_batch(self):
+        if self.include_leftover:
+            return self.idx_in_epoch + 1 < self.num_examples
+        return self.idx_in_epoch + self.batch_size <= self.num_examples
 
-    def rewind(self):
-        self._index_in_epoch = 0
-        self._epochs_completed += 1
-        xqys = zip(self.xs, self.qs, self.ys)
-        np.random.shuffle(xqys)
-        self.xs, self.qs, self.ys = zip(*xqys)
+    def complete_epoch(self):
+        self.reset()
+        self.num_epochs_completed += 1
+
+    def reset(self):
+        self.idx_in_epoch = 0
+        np.random.shuffle(self.idxs)
 
 # TODO : split data into val
 
@@ -83,11 +92,8 @@ def read_babi_files(file_paths):
     return vocab_set, paragraphs, questions, answers
 
 
-def read_babi_split(*file_paths_list):
+def read_babi_split(batch_size, *file_paths_list):
     vocab_set_list, paragraphs_list, questions_list, answers_list = zip(*[read_babi_files(file_paths) for file_paths in file_paths_list])
-    max_sentence_size = max(max(len(sentence) for paragraphs in paragraphs_list for paragraph in paragraphs for sentence in paragraph),
-                            max(len(question) for questions in questions_list for question in questions))
-
     vocab_set = vocab_set_list[0]
     vocab_map = dict((v, k+1) for k, v in enumerate(sorted(vocab_set)))
     vocab_map["<UNK>"] = 0
@@ -101,12 +107,15 @@ def read_babi_split(*file_paths_list):
     qs_list = [[[_get(vocab_map, word) for word in question] for question in questions] for questions in questions_list]
     ys_list = [[_get(vocab_map, answer) for answer in answers] for answers in answers_list]
 
-    data_sets = [DataSet(xs, qs, ys, max_sentence_size=max_sentence_size, vocab_size=len(vocab_map), vocab_map=vocab_map)
+    data_sets = [DataSet(batch_size, range(len(xs)), xs, qs, ys)
                  for xs, qs, ys in zip(xs_list, qs_list, ys_list)]
+    # just for debugging
+    for data_set in data_sets: data_set.vocab_map = vocab_map
     return data_sets
 
 
-def read_babi(dir_path, prefix="", suffix=""):
+def read_babi(batch_size, dir_path, task, suffix=""):
+    prefix = "qa%s_" % str(task)
     train_file_paths = []
     test_file_paths = []
     for file_name in os.listdir(dir_path):
@@ -115,22 +124,27 @@ def read_babi(dir_path, prefix="", suffix=""):
             train_file_paths.append(file_path)
         elif file_name.startswith(prefix) and file_name.endswith(suffix + "_test.txt"):
             test_file_paths.append(file_path)
-    return read_babi_split(train_file_paths, test_file_paths)
+    return read_babi_split(batch_size, train_file_paths, test_file_paths)
 
 
 def split_val(data_set, ratio):
     end_idx = int(data_set.num_examples * (1-ratio))
-    left = DataSet(data_set.xs[:end_idx], data_set.qs[:end_idx], data_set.ys[:end_idx],
-                   max_sentence_size=data_set.max_sentence_size, vocab_size=data_set.vocab_size, vocab_map=data_set.vocab_map)
-    right = DataSet(data_set.xs[end_idx:], data_set.qs[end_idx:], data_set.ys[end_idx:],
-                    max_sentence_size=data_set.max_sentence_size, vocab_size=data_set.vocab_size, vocab_map=data_set.vocab_map)
+    left = DataSet(data_set.batch_size, range(end_idx), data_set.xs[:end_idx], data_set.qs[:end_idx], data_set.ys[:end_idx])
+    right = DataSet(data_set.batch_size, range(len(data_set.xs) - end_idx), data_set.xs[end_idx:], data_set.qs[end_idx:], data_set.ys[end_idx:])
     return left, right
 
 
+def get_max_sizes(*data_sets):
+    max_sent_size = max(len(s) for ds in data_sets for idx in ds.idxs for s in ds.xs[idx])
+    max_ques_size = max(len(ds.qs[idx]) for ds in data_sets for idx in ds.idxs)
+    return max_sent_size, max_ques_size
+
+
 if __name__ == "__main__":
-    train, test = read_babi("data/tasks_1-20_v1-2/en", prefix="qa1_")
+    train, test = read_babi(32, "data/tasks_1-20_v1-2/en", 1)
     # print train.vocab_size, train.max_m_len, train.max_s_len, train.max_q_len
     # print test.vocab_size, test.max_m_len, test.max_s_len, test.max_q_len
-    x_batch, q_batch, y_batch = train.next_batch(10)
-    print x_batch
-    pprint(train.vocab_map)
+    x_batch, q_batch, y_batch = train.get_next_labeled_batch()
+    max_sent_size, max_ques_size = get_max_sizes(train, test)
+    print max_sent_size, max_ques_size
+    # print x_batch
